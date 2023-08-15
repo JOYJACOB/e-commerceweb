45e5f327b4ed0ad4e39830d1c27cc7ac7fd14d86
Jacob <Jacob@DESKTOP-IVLNUPJ>
2023-08-15 11:09:26 +0530

    first Commit

---
+++ ShoppingCart/src/main/java/servlet/org/CancelOrderServlet.java
@@ -0,0 +1,33 @@
+package servlet.org;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+
+import shoppingconnector.org.DbCon;
+import shoppingdao.org.OrderDao;
+@WebServlet("/cancel-order")
+public class CancelOrderServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try(PrintWriter out = response.getWriter()) {
+			String id = request.getParameter("id");
+			if(id != null) {
+				OrderDao orderDao = new OrderDao(DbCon.getConnection());
+				orderDao.cancelOrder(Integer.parseInt(id));
+			}
+			response.sendRedirect("orders.jsp");
+		} catch (ClassNotFoundException|SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+}
---
+++ ShoppingCart/src/main/java/shoppingmodel/org/Cart.java
@@ -0,0 +1,24 @@
+package shoppingmodel.org;
+
+public class Cart extends Product{
+private int quantity;
+	
+
+	
+
+	public Cart() {
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	
+
+}
